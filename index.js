e3370c2d4df81b5676f17edf10935641f1c16925
semenova <a.semenova@pfsz.ru>
2024-02-12 00:40:31 +0700

    added get data from json file

--- index.js
+++ index.js
@@ -1,217 +1,131 @@
-// получаем данные с апи 
 function getDataFromAPI() {
-    return {
-        "services": [
-            {
-                "id": 1,
-                "head": null,
-                "name": "Проф.осмотр",
-                "node": 0,
-                "price": 100.0,
-                "sorthead": 20
-            },
-            {
-                "id": 2,
-                "head": null,
-                "name": "Хирургия",
-                "node": 1,
-                "price": 0.0,
-                "sorthead": 10
-            },
-            {
-                "id": 25,
-                "head": 1,
-                "name": "ТЕСТ",
-                "node": 0,
-                "price": 0.0,
-                "sorthead": 10
-            },
-            {
-                "id": 26,
-                "head": 1,
-                "name": "ТЕСТ",
-                "node": 0,
-                "price": 0.0,
-                "sorthead": 10
-            },
-            {
-                "id": 3,
-                "head": 2,
-                "name": "Удаление зубов",
-                "node": 1,
-                "price": 0.0,
-                "sorthead": 10
-            },
-            {
-                "id": 4,
-                "head": 3,
-                "name": "Удаление зуба",
-                "node": 0,
-                "price": 800.0,
-                "sorthead": 10
-            },
-            {
-                "id": 5,
-                "head": 3,
-                "name": "Удаление 8ого зуба",
-                "node": 0,
-                "price": 1000.0,
-                "sorthead": 30
-            },
-            {
-                "id": 6,
-                "head": 3,
-                "name": "Удаление осколка зуба",
-                "node": 0,
-                "price": 2000.0,
-                "sorthead": 20
-            },
-            {
-                "id": 7,
-                "head": 2,
-                "name": "Хирургические вмешательство",
-                "node": 0,
-                "price": 200.0,
-                "sorthead": 10
-            },
-            {
-                "id": 8,
-                "head": 2,
-                "name": "Имплантация зубов",
-                "node": 1,
-                "price": 0.0,
-                "sorthead": 20
-            },
-            {
-                "id": 9,
-                "head": 8,
-                "name": "Коронка",
-                "node": 0,
-                "price": 3000.0,
-                "sorthead": 10
-            },
-            {
-                "id": 10,
-                "head": 8,
-                "name": "Слепок челюсти",
-                "node": 0,
-                "price": 500.0,
-                "sorthead": 20
-            },
-        ]
-    }
-    ;
-}
+    return new Promise((resolve, reject) => {
+        document.addEventListener("DOMContentLoaded", function () {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'api/api.json', false);
+            xhr.send();
 
-/**
- * Функция для построения дерева
- * @param {*} data 
- * @returns 
- */
-function buildTree(data) {
-    let tree = document.createElement('ul');
-    tree.classList.add('list')
-
-    data.services
-        .filter(service => service.head === null) // Выбираем корневые узлы
-        .sort((a, b) => a.sorthead - b.sorthead) // Сортируем
-        .forEach(service => {
-            const listItem = document.createElement('li');
-        
-            listItem.innerHTML =  service.name + " " + (service.price === 0 ? " " : service.price + "р.");
-            listItem.classList.add('list-item');
-
-            const toggleIcon = document.createElement('i');
-            listItem.prepend(toggleIcon);
-
-            if (service.node === 1) { // если это узел
-                listItem.style.cursor = 'pointer';
-                toggleIcon.classList.add("arrow-right");
-                const subtree = buildSubtree(data.services, service.id); // строим вложенное дерево
-                subtree.hidden = "hidden"; // Скрываем поддерево по умолчанию
-                listItem.appendChild(subtree); // добавляем в DOM
-                }
-             
-            tree.appendChild(listItem); // добавляем в DOM
-        });
-
-    return tree;
-}
-
-
-/**
- * Функция для построения поддерева
- * @param {*} services 
- * @param {*} parentId 
- * @returns 
- */
-function buildSubtree(services, parentId) {
-    const subtree = document.createElement('ul');
-    subtree.classList.add('list');
-
-    services
-        .filter(service => service.head === parentId)
-        .sort((a, b) => a.sorthead - b.sorthead)
-        .forEach(service => {
-            const listItem = document.createElement('li');
-            listItem.innerHTML =  service.name + " " + (service.price === 0 ? " " : service.price + "р.");
-            listItem.classList.add('list-item');
-            const toggleIcon = document.createElement('i');
-            listItem.prepend(toggleIcon);
-
-            if (service.node === 1) {
-                toggleIcon.classList.add("arrow-right");
-                const nestedSubtree = buildSubtree(services, service.id);
-                nestedSubtree.hidden = "hidden";             // скрываем поддерево по умолчанию
-                listItem.appendChild(nestedSubtree);
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                console.log("получил данные: ", data);
+            } else {
+                reject('Ошибка при загрузке данных из файла api.json');
             }
-
-            subtree.appendChild(listItem);
         });
-
-    return subtree;
+    });
 }
 
 
+const data = getDataFromAPI().then(data => {
+    const tree = buildTree(data);
 
+    /**
+     * Функция для построения дерева
+     * @param {*} data 
+     * @returns 
+     */
+    function buildTree(data) {
+        let tree = document.createElement('ul');
+        tree.classList.add('list')
 
-const data = getDataFromAPI(); // получаем данные с апи
+        console.log(data)
 
-const tree = buildTree(data);    // строим дерево
-document.body.appendChild(tree); // добавляем на страницу
+        data.services
+            .filter(service => service.head === null) // Выбираем корневые узлы
+            .sort((a, b) => a.sorthead - b.sorthead) // Сортируем
+            .forEach(service => {
+                const listItem = document.createElement('li');
 
-let list = document.querySelector('.list'); // получаем список 
+                listItem.innerHTML = service.name + " " + (service.price === 0 ? " " : service.price + "р.");
+                listItem.classList.add('list-item');
 
-/**
- * Обрабатываем клик по списку
- * @param {*} event 
- * @returns 
- */
-list.onclick = function(event) {
-    let childrenList = event.target.querySelector('ul');
+                const toggleIcon = document.createElement('i');
+                listItem.prepend(toggleIcon);
 
-    if (!childrenList) return;
+                if (service.node === 1) { // если это узел
+                    listItem.style.cursor = 'pointer';
+                    toggleIcon.classList.add("arrow-right");
+                    const subtree = buildSubtree(data.services, service.id); // строим вложенное дерево
+                    subtree.hidden = "hidden"; // Скрываем поддерево по умолчанию
+                    listItem.appendChild(subtree); // добавляем в DOM
+                }
 
-    childrenList.hidden = !childrenList.hidden;
+                tree.appendChild(listItem); // добавляем в DOM
+            });
 
-    let icon = event.target.querySelector("i");
-
-    if (childrenList.hidden) {
-        event.target.classList.add('hide');
-        event.target.classList.remove('show');
-
-        icon.classList.remove("arrow-down");
-        icon.classList.add("arrow-right");
-
-    } else {
-        event.target.classList.add('show');
-        event.target.classList.remove('hide');
-
-        icon.classList.remove("arrow-right");
-        icon.classList.add("arrow-down");
+        return tree;
     }
 
+    document.body.appendChild(tree); // добавляем на страницу
 
-}
+    /**
+     * Функция для построения поддерева
+     * @param {*} services 
+     * @param {*} parentId 
+     * @returns 
+     */
+    function buildSubtree(services, parentId) {
+        const subtree = document.createElement('ul');
+        subtree.classList.add('list');
+
+        services
+            .filter(service => service.head === parentId)
+            .sort((a, b) => a.sorthead - b.sorthead)
+            .forEach(service => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = service.name + " " + (service.price === 0 ? " " : service.price + "р.");
+                listItem.classList.add('list-item');
+                const toggleIcon = document.createElement('i');
+                listItem.prepend(toggleIcon);
+
+                if (service.node === 1) {
+                    toggleIcon.classList.add("arrow-right");
+                    const nestedSubtree = buildSubtree(services, service.id);
+                    nestedSubtree.hidden = "hidden";             // скрываем поддерево по умолчанию
+                    listItem.appendChild(nestedSubtree);
+                }
+
+                subtree.appendChild(listItem);
+            });
+
+        return subtree;
+    }
+
+    let list = document.querySelector('.list'); // получаем список 
+
+    /**
+     * Обрабатываем клик по списку
+     * @param {*} event 
+     * @returns 
+     */
+    list.onclick = function (event) {
+        let childrenList = event.target.querySelector('ul');
+
+        if (!childrenList) return;
+
+        childrenList.hidden = !childrenList.hidden;
+
+        let icon = event.target.querySelector("i");
+
+        if (childrenList.hidden) {
+            event.target.classList.add('hide');
+            event.target.classList.remove('show');
+
+            icon.classList.remove("arrow-down");
+            icon.classList.add("arrow-right");
+
+        } else {
+            event.target.classList.add('show');
+            event.target.classList.remove('hide');
+
+            icon.classList.remove("arrow-right");
+            icon.classList.add("arrow-down");
+        }
+    }
+
+}).catch(error => {
+    console.error(error);
+});
+
 
 
